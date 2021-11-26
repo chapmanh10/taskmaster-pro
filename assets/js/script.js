8b096b122e43950847145d2c589e615478fa03cd
Houston Shane <chapmanh10@gmail.com>
2021-11-26 10:11:13 -0500

    drag and drop to remove, mobile view

--- assets/js/script.js
+++ assets/js/script.js
@@ -158,7 +158,37 @@ $(".card .list-group").sortable({
     console.log("out", event.target);
   },
   update: function(event) {
-    console.log("update", this);
+// array to store the task data in
+var tempArr = [];
+
+// loop over current set of children in sortable list
+$(this).children().each(function() {
+  var text = $(this)
+    .find("p")
+    .text()
+    .trim();
+
+  var date = $(this)
+    .find("span")
+    .text()
+    .trim();
+
+  // add task data to the temp array as an object
+  tempArr.push({
+    text: text,
+    date: date
+  });
+});
+
+// trim down list's ID to match object property
+var arrName = $(this)
+  .attr("id")
+  .replace("list-", "");
+
+// update array on tasks object and save
+tasks[arrName] = tempArr;
+saveTasks();
+console.log(tempArr);
   }
 });
 
@@ -209,3 +239,17 @@ $("#remove-tasks").on("click", function () {
 loadTasks();
 
 
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    console.log("drop");
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
